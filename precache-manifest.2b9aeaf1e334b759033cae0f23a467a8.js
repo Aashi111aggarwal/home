ee0f908e5e36103ecd02d8bd9e79f005c7fca75f
Naqeeb Ahmed <naqeeb532@outlook.com>
2021-08-23 16:21:03 +0530

    Updates

---
+++ precache-manifest.2b9aeaf1e334b759033cae0f23a467a8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ac1306c7bc85b3e75d7dc25999ec94a",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "434ee5cd2029d943e01c",
+    "url": "/home/static/css/main.e16032d7.chunk.css"
+  },
+  {
+    "revision": "1390d13147fcc8767529",
+    "url": "/home/static/js/2.95a996af.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/home/static/js/2.95a996af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "434ee5cd2029d943e01c",
+    "url": "/home/static/js/main.10f69706.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "7d8b7f06dce76a80716a1a8342115d9d",
+    "url": "/home/static/media/naqeeb.7d8b7f06.jpg"
+  },
+  {
+    "revision": "ebd71f6bb602632dc071673474911a7b",
+    "url": "/home/static/media/resume.ebd71f6b.pdf"
+  }
+]);
